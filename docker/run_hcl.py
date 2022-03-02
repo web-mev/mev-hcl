158a65a6415f03458b498571f1888ae05a981d5e
Brian Lawney <brianlawney@hsph.harvard.edu>
2022-03-01 20:21:57 -0500

    Adding a maximum depth to the output trees. Saves time/space and provides more manageable data for the frontend

--- docker/run_hcl.py
+++ docker/run_hcl.py
@@ -50,6 +50,13 @@ def parse_args():
         help='Name of the output file for the clustered features (if any)'
     )
 
+    parser.add_argument('-n', '--max-depth',
+        dest = 'max_depth',
+        required=False,
+        default = 20,
+        help='The maximum depth of the resulting tree output.'
+    )
+
     args = parser.parse_args()
     return args
 
@@ -75,7 +82,7 @@ def create_linkage_matrix(clustering):
         counts]).astype(float)
 
 
-def print_tree(nodelist, mapping):
+def print_tree(nodelist, node_subset, collapsed_subtrees, mapping):
     '''
     Returns a dict which has nested formatting amenable to plotting a tree 
     structure with, e.g., the D3 javascript library.
@@ -85,6 +92,9 @@ def print_tree(nodelist, mapping):
     with dicts
 
     `nodelist` is a list of ClusterNode instances returned by the `to_tree` function
+    `node_subset` is a list of the nodes from the cut tree. Any nodes lower than our cut
+    point have been removed by collapsing the subtree.
+    `collapsed_subtrees` is a dict that maps a node ID to all the leaves of the subtree. 
     `mapping` is a dict of integers to sample names (e.g. {0: 'A', 1: 'B',...})
     where the integer refers to the column position of the original matrix
     (and hence the linkage) 
@@ -93,15 +103,38 @@ def print_tree(nodelist, mapping):
     scipy returns a list ordered in a manner that should not require multiple visits 
     to the nodes.
     '''
-
-    # Split out which nodes are leaves and which aren't
+    # Split out which nodes are leaves and which aren't.
+    # According to the scipy docs, the first `n` items of `nodelist` are
+    # the leaves, where `n` is the number of items we are clustering
     completed_subtrees = {}
-    others_dict = {}
-    for node in nodelist:
-        if node.is_leaf():
-            completed_subtrees[node.id] = {'name': mapping[node.id]}
+    N = len(mapping)
+
+    # First, take the collapsed subtrees and map the integer node IDs to
+    # the actual sample name
+    for k, collapsed_node_ids in collapsed_subtrees.items():
+        if len(collapsed_node_ids) == 1:
+            # a leaf of the original tree is trivially "collapsed"
+            # the node only has the label and no children.
+            completed_subtrees[k] = {'name': mapping[k]}
         else:
-            others_dict[node.id] = (node.left.id, node.right.id)
+            # a non-leaf node with >= 1 children
+            completed_subtrees[k] = {
+                'name': str(k),
+                'children': [
+                    {'name': mapping[x]} for x in collapsed_node_ids
+                ]
+            }
+
+    # a dictionary which tracks each intermediate node and the left and right immediate children
+    others_dict = {}
+    for node in nodelist[N:]:
+        # if the current node already exists in the `completed_subtrees` dict, then
+        # we don't need to track its descendents since that subtree has already been
+        # processed.
+        if not node.id in completed_subtrees:
+            # we only care about nodes which are above the cut point.
+            if node.id in node_subset:
+                others_dict[node.id] = (node.left.id, node.right.id)
 
     unresolved_nodes = deque(others_dict)
     while unresolved_nodes:
@@ -124,13 +157,65 @@ def print_tree(nodelist, mapping):
             unresolved_nodes.append(node_id)
     return completed_subtrees
 
-def create_tree(linkage_matrix, node_labels):
+
+def walk(root_node, max_depth = 20, level = 0):
+    '''
+    Performs a limited DFS to the max depth. The full tree can be quite deep,
+    which is of limited value for interpretation anyway. By limiting the depth,
+    we can avoid excessively large or deep data structures which can cause
+    issues, e.g. with writing/reading json
+
+    Returns a tuple where the first item is a list of the 
+    current node and all descendents (including intermediate, non-leaf nodes). 
+    The second item is a mapping of the current node to a list of the leaves of
+    that subtree.
+    '''
+    if level < max_depth:
+        # if we are at a leaf, it's trivial- the only "descendent" is itself.
+        if root_node.is_leaf():
+            return [root_node.id], {root_node.id: [root_node.id]}
+        
+        # Otherwise, dive deeper through recursion. The `level` keeps us from going all the way
+        # down and exceeding the default recursion depth limit.
+        left_node_ids, lhs_collapsed_subtrees = walk(root_node.get_left(), level = level + 1)
+        right_node_ids, rhs_collapsed_subtrees = walk(root_node.get_right(), level = level + 1)
+        collapsed_subtrees = {**lhs_collapsed_subtrees, **rhs_collapsed_subtrees}
+        return [
+            root_node.id,
+            *left_node_ids,
+            *right_node_ids
+        ], collapsed_subtrees
+    else:
+        # hit the deepest point we want to go. To avoid walking this tree twice, we 
+        # get the samples in this subtree using the `pre_order` method which returns a
+        # list of all the leaves of the subtree rooted at this node.
+        l = root_node.pre_order(lambda x: x.id)
+        return [root_node.id], {root_node.id: l}
+
+
+def create_tree(linkage_matrix, node_labels, max_depth):
     '''
     Creates a JSON structure amenable for plotting with a library
     such as D3.
     '''
+
+    # root_node is a reference to the ClusterNode instance at the root of the entire tree.
+    # nodelist is a list of ClusterNode instances. Per the docs 
+    # (https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.to_tree.html#scipy.cluster.hierarchy.to_tree)
+    # the first n entries of nodelist correspond to the leaves.
     root_node, nodelist = to_tree(linkage_matrix, rd=True)
-    d = print_tree(nodelist, node_labels)
+
+    # we want to avoid issues with excessively deep trees. These can cause recursion-depth issues when writing/reading the tree in
+    # JSON format. In any case, we are ultimately building this tool to work with a graphical frontend, so we need to be sensitive
+    # to the visualization aspect. Hence, we will perform a cut and simply merge subtrees below the cut.
+    # The `walk` function does this for us.
+    # `nodes` is a list of node identifiers up to some maximum depth. It includes all nodes (leaf and non-leaf) up to 
+    # and including some maximum depth in the tree.
+    # `collapsed_subtrees` is a dict that maps node ids to a list of all the leaves in the subtree. It essentially flattens the subtree
+    nodes, collapsed_subtrees = walk(root_node, max_depth = max_depth)
+
+    d = print_tree(nodelist, nodes, collapsed_subtrees, node_labels)
+
     # dictionary returned by `print_tree` has only a single key addressed by
     # the root node's ID. We only want that part, not the whole dict
     return d[root_node.id]
@@ -213,12 +298,12 @@ if __name__ == '__main__':
     if (args.cluster_dim == 'features') or (args.cluster_dim == 'both'):
         feature_clustering = cluster(df, args.dist_metric, args.linkage)
         feature_linkage_mtx = create_linkage_matrix(feature_clustering)
-        t = create_tree(feature_linkage_mtx, feature_mapping)
+        t = create_tree(feature_linkage_mtx, feature_mapping, args.max_depth)
         feature_tree_output = os.path.join(working_dir, args.features_output)
         json.dump(t, open(feature_tree_output, 'w'))
     if (args.cluster_dim == 'observations') or (args.cluster_dim == 'both'):
         observation_clustering = cluster(df.T, args.dist_metric, args.linkage)
         observation_linkage_mtx = create_linkage_matrix(observation_clustering)
-        t = create_tree(observation_linkage_mtx, obs_mapping)
+        t = create_tree(observation_linkage_mtx, obs_mapping, args.max_depth)
         observation_tree_output = os.path.join(working_dir, args.observations_output)
         json.dump(t, open(observation_tree_output, 'w'))
